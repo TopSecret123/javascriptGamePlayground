e3f213aadf95c6bf026596ec7d5f115345e3b36f
BigPineapple-maker <dasca.chris@gmail.com>
2026-05-26 07:38:42 +1000

    New Origin Needs to be updated. Logic not quite correct as bug is causing some balls to not respect container boundary so need to check logic.

--- script.js
+++ script.js
@@ -12,6 +12,132 @@ let y = 0;
 let intervalId = "";
 
 /**
+ * CollisionManager
+ */
+class CollisionManager {
+	constructor (balls, container, timestamp) {
+		this.balls = balls;
+		this.container = container;
+		this.timestamp = timestamp;
+		this.collisionCards = [];
+	}
+
+	//resolve collisions.
+		//Detect collision. 
+		//Determine new velocities
+		//Give ball new velocity.
+
+		resolveCollisions = () => {
+			//for each ball.
+			//detect collision.
+				//if true 
+					//find first collision.
+					//get velocities of each.
+					//update velocities accordingly
+			//create new array so I can pop off if true.
+			this.balls.forEach((ball) => {
+				this.detectCollision(ball, this.container, this.timestamp) // boolean says yes true.
+				
+			})
+		}
+
+	detectCollision = (ball, container, timestamp) => {
+		//getting ball center location.
+		//going through rest of balls.
+		//But avoid double checking otherwise ball1 will resolve and then ball 4 will undo the resolve.
+		//Wall collision first
+		
+		
+		const {left, top, width, height} = ball.el.getBoundingClientRect()
+		//Find the exact mid point of circle on both x and y plane
+		const xMid = left + (0.5 * width)
+		const yMid = top + (0.5 * height)
+		//exact circle so radius = 1/2 diam which is 1/2 width (or height)
+		const radius = 0.5 * width
+		//wall is simple -> just radius vs wall x or y 
+		
+		const {left: wLeft, top: wTop, right: wRight, bottom: wBottom} = container.getBoundingClientRect()
+		const bounds = container.getBoundingClientRect()
+		
+		console.log("wLeft")
+		console.log(wLeft)
+		//check each four walls.
+		//create wallObj just in case of collision (not optimal but not big problem.)
+		const wall = {mass: Infinity, xVel: 0, yVel: 0, }
+		console.log("xMid");
+		console.log(xMid)
+		console.log(radius)
+		console.log(left)
+		if((xMid - radius) <= wLeft ) {
+			//Need to add collision location.
+			this.collisionCards.push(new CollisionCard(ball, wall, "x", wLeft));
+			console.log(this.collisionCards)
+			//ball.setXVel(-ball.xVel)
+			//ball.xOrigin = wLeft;
+			//ball.xStart = timestamp;
+		}
+		if((xMid + radius) > wRight) {
+			this.collisionCards.push(new CollisionCard(ball, wall, "x", wRight));
+		}
+		if((yMid - radius) <= wTop) {
+			this.collisionCards.push(new CollisionCard(ball, wall, "y", wTop));
+		}
+		if((yMid + radius) >= wBottom) {
+			this.collisionCards.push(new CollisionCard(ball, wall, "y", wBottom));
+		}
+		console.log("Cards Length")
+		console.log(this.collisionCards.length)
+		if(this.collisionCards.length == 0) {
+			console.log("No cards")
+		}
+		else {
+			this.collisionCards.forEach((card) => this.updateVelocity(card))
+		}
+		
+	}
+
+	updateVelocity = (card) => {
+		console.log("updateVel")
+		const {obj1, obj2, collisionAxis: axis, location} = card
+		if(axis == "x") {
+			//console.log(`${obj1.el.id} x collision | vel before: ${obj1.getXVel()} | vel after: ${-obj1.getXVel()}`)
+			obj1.setXVel(-obj1.getXVel());
+			obj1.setXStart(this.timestamp);
+			if(location < 100) {
+				obj1.setXOrigin(location + obj1.radius + 5)
+			}
+			else {
+				obj1.setXOrigin(location - obj1.radius - 5)
+			}
+				
+			//obj1.setXOrigin(obj2.getXPos());
+		}
+		if(axis == "y") {
+			//console.log(`${obj1.el.id} y collision | vel before: ${obj1.getYVel()} | vel after: ${-obj1.getYVel()}`)
+			obj1.setYVel(-obj1.getYVel());
+			obj1.setYStart(this.timestamp);
+			obj1.setYOrigin(obj1.getYPos());
+		}
+	}
+
+
+}
+
+//interfect obj 1 and obj2 (mass, xVel, yVel)
+//if mass is infinite then must be call.
+class CollisionCard {
+	constructor (obj1, obj2, collisionAxis, collisionLocation) {
+		this.obj1 = obj1;
+		this.obj2 = obj2;
+		this.collisionAxis = collisionAxis;
+		this.location = collisionLocation;
+	}
+	//Each object must have an element
+	//For now we will just clash with wall.
+	//Eventually use Collision Card to crash with other balls so need to include colliding object type.
+}
+
+/**
  * Ball Handler has balls. Moves Balls. 
  * Ball container -> hmm who should have that. Needs to be some connection between balls and ball container. 
  * For now -> Detect collisions and knows the boundary -> refactor when necessary.
@@ -20,22 +146,28 @@ class BallHandler {
 	constructor () {
 		this.balls = [];
 		this.ballContainer = ballContainer;
+		this.flag = false;
 	}
 
 	addBall = (ball) => {
 		this.balls.push(ball)
-		ball.startMoving();
+		if(this.balls.length == 1 ) this.startMoving();
 	}
 
-	//resolve collisions.
-		//Detect collision. 
-		//Determine new velocities
-		//Give ball new velocity.
+	resolveCollisions = (timestamp) => {
+		console.log(this.ballContainer)
+		const collisionManager = new CollisionManager(this.balls, this.ballContainer, timestamp);
+		collisionManager.resolveCollisions();
+	}
 
 	moveBalls = (timestamp) => {
-		this.balls.forEach((ball) => ball.move(timestamp))
-		//resolve collisions
-		requestAnimationFrame(this.moveBalls)
+		 
+		this.balls.forEach((ball) => {
+			ball.move(timestamp)
+		})
+		this.resolveCollisions(timestamp);
+		if(!this.flag) requestAnimationFrame(this.moveBalls)
+	
 	}
 
 	startMoving = () => {
@@ -56,12 +188,12 @@ class BallHandler {
 		newDiv.setAttribute("class", "moving-ball")
 		newDiv.setAttribute("id", newId)
 		//Set positioning
-		newDiv.style.left = left + 200 + "px";
-		newDiv.style.top = top + 200 + "px";
+		newDiv.style.left = left + 400 + "px";
+		newDiv.style.top = top + 400 + "px";
 		//add Div and Ball to their homes (html and js)
 		this.ballContainer.appendChild(newDiv)
-		this.addBall(new movingBallC(newDiv))
-		if(this.balls.length == 1) this.startMoving();
+		this.addBall(new MovingBall(newDiv))
+		//if(this.balls.length == 1) this.startMoving();
 	}
 
 
@@ -71,23 +203,40 @@ class BallHandler {
 /**
  * lastMove
  */
-class movingBallC {
+class MovingBall {
 	constructor (el) {
 		this.start = null;
 		this.el = el
 		this.xPos = this.el.getBoundingClientRect().x
 		this.yPos = this.el.getBoundingClientRect().y
-		this.xVel = this.initialVel();
-		this.yVel = this.initialVel();
+		this.xVel =  this.initialVel();
+		this.yVel =  this.initialVel();
 		this.xOrigin = this.el.getBoundingClientRect().x
 		this.yOrigin = this.el.getBoundingClientRect().y
 		this.xStart;
 		this.yStart;
 		this.mass = 1;
+		this.radius = this.el.getBoundingClientRect().width * 0.5
 	}
 	
 	setXVel = (xVel) => {
 		this.xVel = xVel;
+	}
+
+	setXOrigin = (xOrigin) =>{
+		this.xOrigin = xOrigin;
+	}
+
+	setXStart = (xStart) => {
+		this.xStart = xStart;
+	}
+
+	setYOrigin = (yOrigin) => {
+		this.yOrigin = yOrigin;
+	}
+
+	setYStart = (yStart) => {
+		this.yStart = yStart;
 	}
 
 	setYVel = (yVel) => {
@@ -96,6 +245,22 @@ class movingBallC {
 
 	getVelocity = () => {
 		return {xVel: this.xVel, yVel: this.yVel};
+	}
+
+	getXVel = () => {
+		return this.xVel;
+	}
+
+	getYVel = () => {
+		return this.yVel;
+	}
+
+	getXPos = () => {
+		return this.xPos;
+	}
+
+	getYPos = () => {
+		return this.yPos;
 	}
 
 	startMoving = () => {
@@ -150,12 +315,14 @@ class movingBallC {
 		if(!this.yStart) this.yStart = timestamp;
 		const newXPos = this.determineNewPos({pos: this.xPos, vel: this.xVel, start: this.xStart, origin: this.xOrigin}, timestamp);
 		const newYPos = this.determineNewPos({pos: this.yPos, vel: this.yVel, start: this.yStart, origin: this.yOrigin}, timestamp);
-		console.log(newXPos);
-		console.log(newYPos);
 		//Update prevMove to timestamp at end of cycle.
-
+		console.log(this)
+		console.log("xPos")
+		console.log(this.xPos)
 		this.el.style.left = newXPos + "px";
 		this.el.style.top = newYPos + "px"
+		this.xPos = newXPos;
+		this.yPos = newYPos;
 
 		/*
 		if(!this.start) this.start = timestamp;
@@ -236,12 +403,15 @@ const handleClick = (event, source) => {
 			ballHandler.newBall();
 			break;
 		case "stop":
-			stopTimer(intervalId);
+			
+			stopBalls();
 			break;
 	}
 };
 
-
+function stopBalls(){
+	ballHandler.flag = true;
+}
 
 
 
